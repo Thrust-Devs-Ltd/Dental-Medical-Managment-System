df4682254e36ea18de4c8cd73193c780dc14934e
abertnamanya <abertnamanya@gmail.com>
2022-04-01 18:44:09 +0300

    add popup dialog on Invoice showing procedures done

---
+++ resources/views/invoices/invoice_procedures.php
@@ -0,0 +1,19 @@
+<div class="modal fade" id="patient-history-modal" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-full">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+                <h4 class="modal-title">Patient History / <span class="text-primary patientInfoText"></span></h4>
+            </div>
+            <div class="modal-body">
+                <div id="patientHistoryContainer">
+
+                </div>
+                <h3 class="text-primary noResultsText">Patient Has no Treatment History</h3>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn dark btn-outline" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
